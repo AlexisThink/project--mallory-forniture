8ec7bd6151bdd3d462b28795f114dae24b4296fc
Alexis Olveres <alexis.olv.developer@gmail.com>
2018-08-26 18:20:08 -0500

    About Redy

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './about.css'
 
 class About extends React.Component{
     render() {
       return (
-        <div>
-          
+        <div className="container-about">
+          <h1>Created by Alexis Olveres</h1>
         </div>
       )
     }
